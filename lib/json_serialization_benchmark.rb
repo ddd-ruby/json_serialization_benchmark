06b48f5b29ef4d90f0441376e35d2a988bb03267
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-03 16:34:15 +0100

    runner file

--- lib/json_serialization_benchmark.rb
+++ lib/json_serialization_benchmark.rb
@@ -1,5 +1,11 @@
 require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
 
+
+# 3rd party gems
+require 'oj'
+Oj.mimic_JSON() # this will speedup benchmarks using #to_json
+require 'bixby/bench'
+
 require 'active_support/core_ext/object/json'
 
 require 'models/model'
@@ -9,8 +15,10 @@ require 'models/event_factory'
 require 'models/play_by_play_record'
 require 'models/team'
 
+# init files
 require 'rabl/init'
 require 'serializers/init'
+require 'api_view/init'
 
 require 'serializers/event_summary_serializer'
 require 'serializers/team_serializer'
@@ -19,3 +27,5 @@ require 'serializers/basketball/event_serializer'
 require 'presenters/event_summary_presenter'
 require 'presenters/team_presenter'
 require 'presenters/basketball/event_presenter'
+
+require "runner"
